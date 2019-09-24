c880a186f0d6d799811f4d5c5291bd798571ee40
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-09-24 21:11:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do 
-    @text = params[:user_phrase] 
+  post '/piglatinize' do
+    @text = params[:user_phrase]
 
     erb :results
-  end 
-end 
+  end
+end
 
