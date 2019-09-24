e486fef466846bbac8ce7d71949fdb9c49cf69d8
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-09-24 21:13:29 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,17 +5,18 @@ class Piglatinizer
 
     if text.include?(" ")
       split_text(text)
-    elsif ["a", "e", "i", "o", "u"].include?(text[0].downcase) text + "way"
-    else 
-      parts = text.split(/([aeiou].*)/)
-      parts[1]+parts[0]+"ay"
-    end 
-  end 
-  
+    elsif ["a", "e", "i", "o", "u"].include?(text[0].downcase)
+        text + "way"
+    else
+      parts = text.split(/([aeiou].*)/) 
+      parts[1]+parts[0]+"ay" # "ello" + "h" + "ay"
+    end
+  end
+
   def split_text(text)
     words_array = text.split(" ")
     words_array.map do |word|
       piglatinize(word)
-    end.join
-  end 
-end 
+    end.join(" ") 
+  end
+end
