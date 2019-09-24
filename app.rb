602fccd7a538d510657948139cd3fd1a63a75757
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-09-24 21:05:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-end
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do 
+    @text = params[:user_phrase] 
+
+    erb :results
+  end 
+end 
+
---
+++ models/piglatinizer.rb
@@ -0,0 +1,21 @@
+class Piglatinizer 
+  attr_reader :text
+
+  def piglatinize(text)
+
+    if text.include?(" ")
+      split_text(text)
+    elsif ["a", "e", "i", "o", "u"].include?(text[0].downcase) text + "way"
+    else 
+      parts = text.split(/([aeiou].*)/)
+      parts[1]+parts[0]+"ay"
+    end 
+  end 
+  
+  def split_text(text)
+    words_array = text.split(" ")
+    words_array.map do |word|
+      piglatinize(word)
+    end.join
+  end 
+end 
